14d169d7b8a40f8061ad55655782515226347d4a
Alvaro Castillo <alvar.casher19@gmail.com>
2024-09-11 01:03:38 -0600

    1er Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// src/App.tsx
+
+
+import MatrizCalculadora from './components/MatrizCalculadora';
+
+
+function App() {
+  return (
+    <div>
+      <MatrizCalculadora />
+    </div>
+  );
+}
+
+
+export default App;
